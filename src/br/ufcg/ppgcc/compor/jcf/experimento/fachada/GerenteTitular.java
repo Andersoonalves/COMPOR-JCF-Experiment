aef58e2289009003b87b42b73e8b3c9711ad6bf4
andersoonalves <anderson.alves@dce.ufpb.br>
2013-07-13 16:00:54 -0300

    Resposta 1 de andersoonalves

---
+++ src/br/ufcg/ppgcc/compor/jcf/experimento/fachada/GerenteTitular.java
@@ -0,0 +1,88 @@
+package br.ufcg.ppgcc.compor.jcf.experimento.fachada;
+
+import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import br.ufcg.ppgcc.compor.jcf.experimento.util.CalculoImpostoRenda;
+
+
+public class GerenteTitular {
+
+	private List <Titular> titular; 
+	private Map <Titular,List<FontePagadora>> mapFontes;
+	private Map <Titular,List<Dependente>> mapDependentes;
+	private CalculoImpostoRenda calculoImposto;
+	
+	public GerenteTitular (){
+		titular = new ArrayList<Titular>(); 
+		this.mapFontes = new HashMap<Titular, List<FontePagadora>>();
+		this.mapDependentes = new HashMap<Titular, List<Dependente>>();
+		this.calculoImposto = new CalculoImpostoRenda();
+		
+	}
+	
+	 private boolean contensTitular(String tituloEleitoral) {
+	        for (Titular t : titular) {
+	            if (t.getTituloEleitoral().equalsIgnoreCase(tituloEleitoral)) {
+	                return true;
+	            }
+	        }
+	        return false;
+	    }
+	
+	public boolean CriarNovoTitular(Titular t){
+			 if (!contensTitular(t.getTituloEleitoral())) {
+		        	titular.add(t);
+		        }
+		        return false;
+	}
+	public List<Titular> listarTitulares (){
+		 List<Titular> titular = new ArrayList<Titular>();
+	        for (Titular t : this.titular) {
+	                titular.add(t);
+	        }
+	        return titular;
+	}
+	
+	public void criarFontePagadora (Titular titular, FontePagadora fonte) {
+		if(mapFontes.get(titular) == null){
+			mapFontes.put(titular, new ArrayList<FontePagadora>());
+		}
+		mapFontes.get(titular).add(fonte);
+	}
+	
+	public List<FontePagadora> listarFontes(Titular titular) {	
+		return mapFontes.get(titular);
+		
+	}
+	
+	public void criarDependente(Titular titular, Dependente dependente) {
+		if(mapDependentes.get(titular) == null){
+			mapDependentes.put(titular, new ArrayList<Dependente>());
+		}
+		mapDependentes.get(titular).add(dependente);
+	}
+	public List<Dependente> listarDependentes(Titular titular) {
+		if(mapDependentes.get(titular) == null){
+			mapDependentes.put(titular, new ArrayList<Dependente>());
+		}
+		return mapDependentes.get(titular);
+	}
+	
+	public Resultado declaracaoCompleta(Titular titular) {
+		Resultado result = new Resultado();
+		
+		double recebido = calculoImposto.totalRecebido((mapFontes.get(titular)));	
+		
+		recebido = calculoImposto.descontoDependentes(recebido, listarDependentes(titular));
+				
+		result.setImpostoDevido(this.calculoImposto.impostoDevido(recebido));
+		return result;
+	}
+	
+	
+
+}
---
+++ src/br/ufcg/ppgcc/compor/jcf/experimento/impl/FachadaImpl.java
@@ -0,0 +1,62 @@
+package br.ufcg.ppgcc.compor.jcf.experimento.impl;
+
+import java.util.LinkedList;
+import java.util.List;
+
+import br.ufcg.ppgcc.compor.jcf.experimento.fachada.Dependente;
+import br.ufcg.ppgcc.compor.jcf.experimento.fachada.FachadaExperimento;
+import br.ufcg.ppgcc.compor.jcf.experimento.fachada.FontePagadora;
+import br.ufcg.ppgcc.compor.jcf.experimento.fachada.GastoDedutivel;
+import br.ufcg.ppgcc.compor.jcf.experimento.fachada.GerenteTitular;
+import br.ufcg.ppgcc.compor.jcf.experimento.fachada.Pessoa;
+import br.ufcg.ppgcc.compor.jcf.experimento.fachada.Resultado;
+import br.ufcg.ppgcc.compor.jcf.experimento.fachada.Titular;
+
+public class FachadaImpl implements FachadaExperimento{
+
+	
+	private GerenteTitular titular; 
+	
+	public FachadaImpl (){
+		titular = new GerenteTitular(); 
+	}
+	
+	@Override
+	public void criarNovoTitular(Titular titular) {
+			this.titular.CriarNovoTitular(titular);
+	}
+
+	@Override
+	public List<Titular> listarTitulares() {
+			return this.titular.listarTitulares();
+	}
+
+	@Override
+	public void criarFontePagadora(Titular titular, FontePagadora fonte) {
+			this.titular.criarFontePagadora(titular, fonte);
+	}
+
+	@Override
+	public Resultado declaracaoCompleta(Titular titular) {
+		return this.titular.declaracaoCompleta(titular);	
+	}
+
+	@Override
+	public void criarDependente(Titular titular, Dependente dependente) {
+		this.titular.criarDependente(titular, dependente);
+	}
+
+	@Override
+	public List<FontePagadora> listarFontes(Titular titular) {
+		return this.titular.listarFontes(titular);
+	}
+
+	@Override
+	public List<Dependente> listarDependentes(Titular titular) {
+		return this.titular.listarDependentes (titular);
+	}
+
+	
+
+	
+}
